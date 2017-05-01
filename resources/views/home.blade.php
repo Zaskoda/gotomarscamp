ca5fda3a6067f687193bc6c8ccb492f8bd65ec2c
zasKoda <zaskoda@gmail.com>
2017-04-30 21:05:39 -0700

    mars camp initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="home-container">
+    <div class="home-welcome">
+        <div class="wrap cf">
+            <div class="content">
+                <h3>Mars Camp</h3>
+
+                <p>GoTo it... </p>
+            </div>
+        </div>
+    </div>
+    <div class="home-mars1">
+
+        <div class="row">
+            <div class="col-md-8">
+
+                <div class="home-redcontent">
+
+                    <img src="/images/mars-earth-comparison.jpg">
+
+                    <h1>What is Mars Camp?</h1>
+                    <p>Mars Camp is a Burning Man Theme camp planned for 2018. Mars Camp strives to simulate,
+                    to the best of our abilities, the experience of estalishing and living in a base camp
+                    on the surface of our planetary cousin, Mars. While survival at Burning Man may not be 
+                    as extreme as the Red Planet, the venue provides a lovely dusty, windy desert backdrop.
+                    </p>
+
+                    <h1>But Why?</h1>
+                    <p>You really have to ask? Mars Camp is for people who want to go to Mars but will probably
+                    never get the chance. This project invites enthusiasts, artists, and engineers join
+                    the experience. 
+
+                    <h1>Ok, how?</h1>
+                    <p>There are three ways to you can participate. 
+                    <ul>
+                        <li>Core Team:</li>
+                        <li>Regional Project:</li>
+                        <li>Volunteer Registry:</li>
+                    </p>
+                </div>
+
+            </div>
+            <div class="col-md-4">
+                <div class="home-redcontent">
+                    <p><b>Enlist Today</b></p>
+                    <p>
+                        <a href="">Volunteer Signup</a>
+                    </p>
+                    <p>
+                        <a href="">Mailing List</a>
+                    </p>
+                    <p>
+                        <a href="">Facebook</a>
+                    </p>
+                    <p>
+                        <a href="">Twitter</a>
+                    </p>
+                </div>
+
+                <div class="home-redcontent">
+                    <p><b>Latest News</b></p>
+                    <p><b>April 11, 2017:</b><br>Our new website at <a href="http://gotomars.camp">GoToMars.Camp</a> is online!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
