1c4e065cfc1b9ebce9da7dcfe22055811b84eaa9
zasKoda <zaskoda@gmail.com>
2017-05-15 23:46:34 -0700

    better initial site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,60 +14,26 @@
             </div>
         </div>
     </div>
-    <div class="home-mars1 container">
-
-        <div class="row">
-            <div class="col-md-8">
-
-                <div class="home-redcontent">
-
-                    <img src="/images/mars-earth-comparison.jpg">
-
-                    <h1>What is Mars Camp?</h1>
-                    <p>Mars Camp is a Burning Man Theme camp planned for 2018. Mars Camp strives to simulate,
-                    to the best of our abilities, the experience of estalishing and living in a base camp
-                    on the surface of our planetary cousin, Mars. While survival at Burning Man may not be 
-                    as extreme as the Red Planet, the venue provides a lovely dusty, windy desert backdrop.
+    <div class="container">
+        <div class="home-mars1">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+                    <p class="fancy">
+                        <b>What:</b> Mars Camp <br>
+                        <b>Where:</b> Black Rock City <br>
+                        <b>When:</b> 2018<br>
                     </p>
-
-                    <h1>But Why?</h1>
-                    <p>You really have to ask? Mars Camp is for people who want to go to Mars but will probably
-                    never get the chance. This project invites enthusiasts, artists, and engineers join
-                    the experience. 
-
-                    <h1>Ok, how?</h1>
-                    <p>There are three ways to you can participate. 
-                    <ul>
-                        <li>Core Team:</li>
-                        <li>Regional Project:</li>
-                        <li>Volunteer Registry:</li>
-                    </p>
+                    <p>Mars Camp strives to simulate, to the best of our abilities, the experience of living in a basecamp on the surface of our planetary sibling, Mars. While survival at Burning Man may not be as extreme as the Red Planet, the venue provides a lovely, dusty, windy desert backdrop for the experience.</p>
+                    <p>What’s it like to visit Mars camp? Imagine walking up to a tall rocket and entering at the base. After ascending to the top of the launch vehicle, you sit down, lean back, put on your helmet, and buckle your harness. It goes dark. The monitors come on. The countdown begins, 3.. 2.. *everything is shaking* 1… Blast off! … After a simulated flight to mars, it’s time to depart the transport ship. As you exit the lander, you enter an airlock that leads to a tunnel. Through small round windows along the tunnel you catch glimpses of the Martian landscape outside as well as parts of the base. At the end of the tunnel, you’re greeted by Mars Customs agents. Once your arrival is recorded and your interplanetary passport is stamped, you cross through the gates into the community dome, the largest dome in the Mars Camp superstructure. Welcome to Mars. </p>
+                    <p>Before we continue, let me tell you a little more about the infrastructure: All of Mars Camp is contained within one big enclosed superstructure. Each entrance and exit has an airlock protecting the integrity of the station’s artificial atmosphere. The superstructure includes the previously mentioned primary community dome as well as: a kitchen and pantry module, crew quarters, rec rooms, a fabrication and research lab, an observatory, and any number of other modules that can be connected to the superstructure. Modules will connect together using modular hallways and standardized doorway connections. The station will also provide a power grid, water management systems, a wireless communications network, and other life support and service systems.</p>
+                    <p>Back to the experience, once you’re inside the community dome you’re free to explore Mars Camp. There’s about to be a talk on the main stage about Mars related stuff. Before you walk over, you’ll want to grab a smoothie made from hydroponically grown fruits and vegetables. Once the talk is over, they’ll offer a tour of the station. Instead, you head directly over to the observatory. You find a comfy place in the dome to lay down and look up at the Martian night sky. Ah yeah, there’s Earth.</p>
+                    <p>As you head to the airlock, on your way out of the station, you suddenly take a randomly solicited recommendation for a different route. You traverse yet another tunnel and walk into the pressurized garage. Once everyone is on board the Mars-rover-art-car-of-your-dreams and the hatch is closed, the garage opens up and the rover with it’s crew are off to explore deep playa. </p>
+                    <p>More to come,<br>Koda</p>
+                    <p><a href="https://twitter.com/gotomarscamp">Follow @GoToMarsCamp on Twitter</a></p>
+                    <p><a href="https://www.facebook.com/groups/447166618958188/">Join the Mars Camp Facebook Group</a></p>
                 </div>
-
-            </div>
-            <div class="col-md-4">
-                <div class="home-redcontent">
-                    <p><b>Enlist Today</b></p>
-                    <p>
-                        <a href="">Volunteer Signup</a>
-                    </p>
-                    <p>
-                        <a href="">Mailing List</a>
-                    </p>
-                    <p>
-                        <a href="">Facebook</a>
-                    </p>
-                    <p>
-                        <a href="">Twitter</a>
-                    </p>
-                </div>
-
-                <div class="home-redcontent">
-                    <p><b>Latest News</b></p>
-                    <p><b>April 11, 2017:</b><br>Our new website at <a href="http://gotomars.camp">GoToMars.Camp</a> is online!</p>
-                </div>
-            </div>
-        </div>
+            </div>    
+        </div>  
     </div>
 </div>
 
